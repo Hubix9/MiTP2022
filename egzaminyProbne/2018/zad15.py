2f764e65c049455deefe04c96191452945167dcb
Hubix9 <11819738+Hubix9@users.noreply.github.com>
2022-06-14 00:17:06 +0200

    Update zad15.py

--- egzaminyProbne/2018/zad15.py
+++ egzaminyProbne/2018/zad15.py
@@ -7,3 +7,14 @@
 # A
 # H
 # D
+
+# Gotowy kod
+while True:
+    try:
+        num1, num2 = input("Podaj:").split(",")
+        result = int(num1) / int(num2)
+    except:
+        print("Bledne dane.")
+        break
+    else:
+        print("Iloraz", result)
